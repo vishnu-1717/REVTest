d98486eccd2249630c48d25a6b6a8c09097c3809
benblip1 <ben@systemizedsales.com>
2025-11-21 11:55:32 -0500

    Capitalize browser tab title to 'Revphlo'

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "revphlo",
+  title: "Revphlo",
   description: "Sales and commission management platform",
 };
 
